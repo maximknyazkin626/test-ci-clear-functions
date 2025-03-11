82ab96607f1a0e760335bf26c91f7afd62a77637
knyazkin maxim <knyazkin.maxim.626@gmail.com>
2025-03-11 21:31:06 +0300

    appveyor commit

--- src/test.index.js
+++ src/test.index.js
@@ -1,6 +1,6 @@
-const { checkHealth } = require("./index.js");
+const checkHealth = require("index.js");
 
-test("checkHealth test", () => {
+test('checkHealth test', () => {
   expect(checkHealth(60)).toBe("healthy");
   expect(checkHealth(35)).toBe("wounded");
   expect(checkHealth(10)).toBe("critical");
